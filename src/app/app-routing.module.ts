b02ef8d8ff0f74eca2c3f200e17e1dd60ea8ffe5
Ken Sodemann <ken@ionic.io>
2022-01-12 10:37:02 -0600

    step(four): mock up the tea page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+    path: '',
+    redirectTo: 'tea',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tea',
+    loadChildren: () => import('./tea/tea.module').then((m) => m.TeaPageModule),
   },
 ];
 
--- src/app/tea/tea-routing.module.ts
+++ src/app/tea/tea-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TeaPage } from './tea.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage,
+    component: TeaPage,
   },
 ];
 
@@ -13,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class TeaPageRoutingModule {}
